39666b65b86a2d857a665a59f0adb257daf4e5fb
Hobo <1830231903@qq.com>
2023-03-04 10:14:16 +0800

    update: add default completion model

--- src/OpenAI.Net/Completions/CompletionRequest.cs
+++ src/OpenAI.Net/Completions/CompletionRequest.cs
@@ -15,7 +15,7 @@ namespace OpenAI.Net.Completions
         /// ID of the model to use. You can use the <see href="https://platform.openai.com/docs/api-reference/models/list">List models</see> API to see all of your available models, or see our 
         /// <see href="https://platform.openai.com/docs/models/overview">Model overview</see> for descriptions of them.
         /// </summary>
-        public string Model { get; set; } = string.Empty;
+        public string Model { get; set; } = "text-davinci-003";
 
         /// <summary>
         /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
